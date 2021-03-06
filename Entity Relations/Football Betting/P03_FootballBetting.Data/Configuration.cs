bdb787a095f459ec2a0a6ac04ed81d3be5876822
AngelVasilev <58532362+AchoVasilev@users.noreply.github.com>
2021-03-06 21:18:44 +0200

    Add files via upload

---
+++ Entity Relations/Football Betting/P03_FootballBetting.Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace P03_FootballBetting.Data
+{
+    internal static class Configuration
+    {
+        internal static string ConnetctionString = @"Server=.;Database=FootballBettingSystem;Integrated Security=true;";
+    }
+}
